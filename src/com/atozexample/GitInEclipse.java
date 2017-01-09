3e74b4820d97f7d43e9c169b282ff46225d4516e
James Bond <ns20092009@live.de>
2017-01-09 13:27:59 +0100

    Added commit in git file.
    Change-Id: I493a92b2f18a2be972e6c1e6eb8a86cdedf5f38b
    Signed-off-by: James Bond <ns20092009@live.de>

--- src/com/atozexample/GitInEclipse.java
+++ src/com/atozexample/GitInEclipse.java
@@ -2,6 +2,7 @@ package com.atozexample;
 
 public class GitInEclipse {
 
+	//This is main method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("This is first clone project clone in Eclipse");
